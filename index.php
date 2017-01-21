efac5730b76fd2c8aadcc82c3da28379fe756264
Kaur [Pawan] <kaurpawanpreet12@gmail.com>
2017-01-21 23:08:34 +0100

    le dossier à jour

--- index.php
+++ index.php
@@ -1,17 +1,28 @@
 <?php
 include('include/class.translator.php');
 $language = new Translator();
-
-
 ?>
 <html>
     <head>
         <title>AfrasiabMarion</title>
+		<link rel="stylesheet" type="text/css" href="design/design.css"/>
     </head>
     <body>
         <center><h3>Translator!</h3></center>
-	<?php
-	$language->getIntro();
-	?>
+		<div id="main_content">
+		<div id="header"></div>
+		<div id="content"></div>
+		<div id="menu">
+			<ul>
+				<li>Home</li>
+				<li>Dictionnary</li>
+				<li>Translator</li>
+				<li>Contact</li>
+				<li>Application</li>	
+			</ul>
+		</div>
+		<div id="footer"></div>		
+		</div>
+	
     </body>
 </html>
